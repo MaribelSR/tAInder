cd26904bceb95a884c79a912204ae8d21321cfcb
Maribel Salvador Rufo <maribelsalvadorr@gmail.com>
2025-07-08 21:32:06 +0200

    docs: Finally models.

--- backend/main/models.py
+++ backend/main/models.py
@@ -13,9 +13,32 @@ class Profile(models.Model):
     last_access = models.DateTimeField()
     tags = models.ManyToManyField(Tag)
 
+class User(models.Model):
+    email = models.CharField(max_length=200, unique=True, null=False)
+    password = models.CharField(max_length=20, null=False)
+
 class Ai(models.Model):
     personality = models.TextField()
     schedule = models.TextField()
     last_execution = models.DateTimeField()
     next_execution = models.DateTimeField()
+class Match(models.Model):
+    #FK tendría que añadir algo :/ dudillas.
+    profile_id_a = models.IntegerChoices()
+    profile_id_b = models.IntegerChoices()
+    do_match_a_b = models.BooleanField()
+    do_match_b_a = models.BooleanField()
+
+class Message(models.Model):
+    message = models.TextField()
+    #Tengo dudas en ambas tanto la de arriba por no limitarlo en char aunque es
+    #un mensaje al final y podria ser char. Y luego, la de abajo por lo del tipo
+    #que hemos puesto int pero no se yo. 
+    type = models.IntegerField()
+    published = models.DateTimeField()
+    deleted = models.BooleanField()
+    replied_message_id = models.IntegerField()
+    profile_id = models.IntegerField()
+    #debo de añadir aquí también que no sea null o a ser FK ya está todo eso donde corresponde?
+    match_id = models.IntegerField()
 
