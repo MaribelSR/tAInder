58c067eed723db25f32c8a01214c58061a2f96e3
Maribel Salvador Rufo <maribelsalvadorr@gmail.com>
2025-07-17 19:41:48 +0200

    backend: Fetch tags from DB for generate_profile
    - Delete unused constants.py

--- backend/main/management/commands/generate_profile.py
+++ backend/main/management/commands/generate_profile.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from main.models import Profile
+from main.models import Profile, TagCategory, Tag
 import requests
 import json
 import random
@@ -8,8 +8,6 @@ import sys
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print("Hello, world!")
-
         """
         print(Profile.objects.all())
         maribel = Profile.objects.get(username="maribelSR")
@@ -34,7 +32,7 @@ class Command(BaseCommand):
         prompt = """
                 Genera un perfil para una aplicación para conocer personas 
                 con los campos, username, first_name, last_name, height, birthday, 
-                description, personality, schedule, next_execution, tags (siguiendo la lista de tags).
+                description, personality, schedule, next_execution.
                 
                 FORMATO DE RESPUESTA: Responde ÚNICAMENTE con un JSON válido con esta estructura exacta:
                 {
@@ -44,7 +42,6 @@ class Command(BaseCommand):
                 "height": 175,
                 "birthday": "1990-05-15",
                 "description": "Descripción en primera persona...",
-                "tags": ["tag1", "tag2", "tag3"]
                 }
 
                 Intrucciones específicas: 
@@ -55,58 +52,63 @@ class Command(BaseCommand):
                 "númeroNombrenúmero".
                 - En España en el campo last_name son dos apellidos y se guardará como texto plano.
                 - El campo descripción debe ser lenguaje coloquial y cercano, en primera persona. Que transmita parte de su personalidad y sus gustos.
-                - El perfil tiene que ser atrayante para una persona de nacionalidad española y residente en España.
-                - El perfil debe de ser equilibrado, comprobando la lista de 100 personas de la base de 
-                datos y haciendo que haya tanto hombres como mujeres de forma equitativa, cada uno únicos.
-                - El campo tags debe ser una lista de strings. 
+                - El perfil tiene que ser atrayante.
+                - El perfil tiene que estar escrito en español.
+               """
+        # TODO elegir 2 tags de categoría sexualidad.
+        tagCategorySexuality = TagCategory.objects.get(name="Sexuality")
+        sexualitiesAsObjects = random.sample(
+            list(tagCategorySexuality.tag_set.all()), k=2
+        )
+        sexualitiesAsStrings = []
+        for sexuality in sexualitiesAsObjects:
+            sexualitiesAsStrings.append(sexuality.name)
+        # sexualitiesAsStrings = [sexuality.name for sexuality in Tag.objects.filter(category__name="Sexuality").order_by("?")[:2]]
+        prompt += "\nSexualidad: {}".format(",".join(sexualitiesAsStrings))
 
-                TAGS OBLIGATORIOS QUE PUEDEN INCLUIR DE FORMA ALEATORIA (HOMBRE/MUJER/OTROS): 
-                - Personalidad: "extrovertido", "introvertido", "tranquilo", "nervioso", "impulsivo", "reflexivo",
-                "agresivo", "sociable", "solitario", "creativo", "realista", "analítico", "pesimista", "optimista",
-                "narcicista", "organizado", "espontáneo", "romántico", "independiente", "ambicioso", "conformista",
-                "responsable", "pacífico", "dependiente", "empático", "sensible", "inteligente", "disciplinado", "compasivo",
-                "servicial", "preocupado", "inestable", "ansioso", "irritable", "cooperativo", "asertivo", "enérgico", 
-                "confiable", "persistente", "dócil", "serio", "aventurero", "apasionado", "maduro", "leal, "ileal", 
-                "deshonesto", "honesto", "metódico", "caótico", "impredecible", "predecible", "reservado", 
-                "impaciente", "paciente", "mentiroso", "directo", "sincero", "competente", "incompetente", "comprensivo", 
-                "prudente", "crítico", "comprometido", "manipulador", "animalista". 
+        # TODO elegir 3-5 tags de categoria hobby
+        tagCategoryHobby = TagCategory.objects.get(name="Hobby")
+        hobbiesAsObjects = random.sample(
+            list(tagCategoryHobby.tag_set.all()), k=random.randint(3, 5)
+        )
+        hobbiesAsStrings = []
+        for hobby in hobbiesAsObjects:
+            hobbiesAsStrings.append(hobby.name)
+        prompt += "\nHobbies: {}".format(",".join(hobbiesAsStrings))
 
-                - Hobbies: "leer", "escribir", "ver series", "ver películar", "cinéfilo", "pintor", "dibujante", "cerámica", "fútbol", "baloncesto"
-                "deportes", "correr", "nadar", "bailar", "tenis", "golf", "padel", "crossfit", "gimnasio", "pilates", "yoga", 
-                "crear contenido", "diseñar", "decorar", "gastronomía", "ajedrez", "coleccionismo", "cocina", "repostería", 
-                "viajar", "artes marciales", "senderismo", "acampada", "pesca", "jardinería", "ciclismo", "surf", "deportes acuáticos", 
-                "patinaje", "esquí", "béisbol", "hockey", "rugby", "escuchar música", "música", "cosplay", "teatro", "animes",
-                "carpintería", "motos", "mecánica", "coches", "programación", "meditar", "escribir blogs", "aprender idiomas", 
-                "ir de compras", "tocar instrumentos musicales", "cantar", "danza", "maquillaje", "conocer lugares nuevos", "visitar museos".
+        # TODO elegir 3-5 tags de categoria personalidad.
+        tagCategoryPersonality = TagCategory.objects.get(name="Personality")
+        personalitiesAsObjects = random.sample(
+            list(tagCategoryPersonality.tag_set.all()), k=random.randint(3, 5)
+        )
+        personalitiesAsStrings = []
+        for personality in personalitiesAsObjects:
+            personalitiesAsStrings.append(personality.name)
+        prompt += "\nPersonalidad: {}".format(",".join(personalitiesAsStrings))
 
-                - Profesión: "médico", "enfermero", "farmacéutico", "fisioterapeuta", "dentista", "veterinario", "psicólogo", "psiquiatra", 
-                "otras ramas de ciencias", "abogado", "jurista", "procurador", "juez", "notario", "ingeniero", "informático", "maestro", "carpintero",
-                "albañil", "fontanero", "cerrajero", "pintor", "mecánico", "sastre", "panadero", "carnicero", "cocinero", "joyero", "alfarero", "tejedor",
-                "vidriero", "científico", "biólogo", "químico", "estadístico", "analista de datos", "programador web", "diseñador gráfico", "diseñador", 
-                "community manager", "influencer", "creador de contenido digital", "arquitecto", "ingeniero industrial", "trabajador social", "educador social", 
-                "sociólogo", "economista", "administrador", "empresario", "periodista", "filólogo", "traductor", "comercial", "ventas", "camarero", 
-                "guía turístico", "recepcionista", "guardia", "bailarin", "policia", "militar", "guardia civil", "estudiante", "sin empleo", "piloto", "chófer", 
-                "camionero", "cirujano", "astrónomo", "autónomo", "modelo", "azafato", "actor", "actriz", "músico", "otros". 
+        # TODO elegir 1-2 tags de categoria profesión.
+        tagCategoryProfession = TagCategory.objects.get(name="Profession")
+        professionsAsObjects = random.sample(
+            list(tagCategoryProfession.tag_set.all()), k=random.randint(1, 2)
+        )
+        professionsAsStrings = []
+        for profession in professionsAsObjects:
+            professionsAsStrings.append(profession.name)
+        prompt += "\nProfesión: {}".format(",".join(professionsAsStrings))
 
-                - Sexualidad: "heterosexual", "homosexual", "bisexual", "asexual", "androginosexual", "antrosexual", "bicurioso", "demisexuales",
-                "poliamorosos", "polisexual", "sapiosexual", "trans", "género binario", "no binario", "queer", "agénero", "bigénero", "género fluido", 
-                "cisgénero", "intersexual", "transgénero", "transexual", "otros". 
+        # TODO elegir 3-5 tags de la categoria estilo de vida.
+        tagCategoryLifeStyle = TagCategory.objects.get(name="Life Style")
+        lifeStylesAsObjects = random.sample(
+            list(tagCategoryLifeStyle.tag_set.all()), k=random.randint(3, 5)
+        )
+        lifeStylesAsStrings = []
+        for lifeStyle in lifeStylesAsObjects:
+            lifeStylesAsStrings.append(lifeStyle.name)
+        prompt += "\nEstilo de vida: {}".format(",".join(lifeStylesAsStrings))
 
-                - Estilo de vida: "urbano", "rural", "madrugador", "noctámbulo", "social", "hogareño", "minimalista", "bohemio", "clásico",
-                "moderno", "eco-friendly", "foodie", "viajero", "casero", "familiar", "saludable", "hábitos saludables", "fiestero", "mochilero".
-
-                Los tags deben reflejarse en la descripción y personalidad del perfil, siguiendo los patrones mencionados.
-                Incluye esos tags en el campo "tags" del Json. 
-            
-                - El perfil generado debe ser diferente, sin coincidencias, únicos, 
-                y teniendo nombre y apellidos diferentes, a los siguientes: 
-                """
-        for profile in Profile.objects.all()[:101]:
-            prompt += "\n-username: {}, first_name: {}, last_name: {}".format(
-                profile.username,
-                profile.first_name,
-                profile.last_name,
-            )
+        prompt += "\n\nIntenta evitar los siguientes nombres:"
+        for profile in Profile.objects.values("first_name").distinct():
+            prompt += "\n-{}".format(profile["first_name"])
 
         print(prompt)
         response = requests.post(
@@ -126,6 +128,7 @@ class Command(BaseCommand):
         print(msg)
         # Parse json to python dict
         profile = json.loads(msg)
+
         Profile.objects.create(
             username=profile["username"],
             first_name=profile["first_name"],
@@ -133,4 +136,10 @@ class Command(BaseCommand):
             height=profile["height"],
             birthday=profile["birthday"],
             description=profile["description"],
+            # TODO asignar tags
+            # tags=sexualitiesAsObjects
+            # + hobbiesAsObjects
+            # + personalitiesAsObjects
+            # + professionsAsObjects
+            # + lifeStylesAsObjects,
         )
