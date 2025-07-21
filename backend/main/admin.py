1309776401c11a2cd4307dd461298d11d20b41b2
Maribel Salvador Rufo <maribelsalvadorr@gmail.com>
2025-07-21 18:10:56 +0200

    backend: Show specific columns for some models

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
 from .models import Profile, Message, Ai, Match, Tag, TagCategory, User
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "first_name",
+        "last_name",
+    ]
+    ordering = ["username"]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = [
+        "category",
+        "name",
+    ]
+    ordering = ["category", "name"]
+
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+
+class TagCategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+    ordering = ["name"]
+    inlines = [TagInline]
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Message)
 admin.site.register(Ai)
 admin.site.register(Match)
-admin.site.register(Tag)
-admin.site.register(TagCategory)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(TagCategory, TagCategoryAdmin)
 admin.site.register(User)
