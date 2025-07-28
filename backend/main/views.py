8af019980f7dbf33b5873040cfe2d22d2f617945
Maribel Salvador Rufo <maribelsalvadorr@gmail.com>
2025-07-28 19:25:48 +0200

    backend: Add Djando Rest Framework

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from main.models import Profile
+
 
 # Create your views here.
+def view_profiles(request):
+    profiles = Profile.objects.all()
+    response = ""
+    for profile in profiles:
+        response += profile.first_name + " " + profile.last_name + "<br>"
+    response = """
+{
+    "profiles": [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+        },
+        {
+           "first_name": "Jane",
+           "last_name": "Smith",
+        }
+    ]
+}
+"""
+    return HttpResponse(response)
