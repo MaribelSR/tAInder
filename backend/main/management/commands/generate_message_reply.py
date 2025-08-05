9da0c26d6fe9e3bbff668b4bf35fa9e73b89498c
Maribel Salvador Rufo <maribelsalvadorr@gmail.com>
2025-08-05 17:47:40 +0200

    backend: Complete generate_message_reply command.
    Close #8

--- backend/main/management/commands/generate_message_reply.py
+++ backend/main/management/commands/generate_message_reply.py
@@ -1,15 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 import requests
-from main.models import Match
+from main.models import Match, Message
+import json
+from datetime import datetime, timezone
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for match in Match.objects.filter(do_match_a_b=True, do_match_b_a=True):
             try:
-                last_msg = match.message_set.exclude(deleted=True).order_by("published")[0]
+                last_msg = match.message_set.exclude(deleted=True).order_by(
+                    "-published"
+                )[0]
                 # Skip Match that last Message is from Ai
                 if last_msg.profile.ai_set.first():
+                    print("Skip {} because the last message its from Ai".format(match))
                     continue
                 # sacar perfil de la Ai
                 ai_profile = None
@@ -23,9 +28,10 @@ class Command(BaseCommand):
 
                 # TODO Llamada a la API de Ollama
                 prompt = """
-                    Interpretando el siguiente perfil: {ai_profile}
+                    Interpretando el siguiente perfil (no incluir descripción ni tags en la respuesta): {ai_profile}
                     Contesta al siguiente mensaje: {last_msg}
                     Perteneciente al usuario: {user_profile}.
+                    Tu respuesta debe seguir el siguiente formato: {{"response": "Contestación"}}
                 """.format(
                     ai_profile=ai_profile.to_json(),
                     last_msg=last_msg.msg,
@@ -46,6 +52,17 @@ class Command(BaseCommand):
                     print(aIresponse)
                     raise CommandError("No se ha generado una respuesta del Ollama")
                 msg = aIresponse["response"]
+                msg = json.loads(msg)
+                msg = msg["response"]
                 print(msg)
+
+                Message.objects.create(
+                    msg=msg,
+                    published=datetime.now(timezone.utc),
+                    deleted=False,
+                    replied_message=last_msg,
+                    profile=ai_profile,
+                    match=match,
+                )
             except Exception as e:
                 print(e)
